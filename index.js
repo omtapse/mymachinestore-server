7fb76ad9052b16092e48db8150b75db4ae47c3d0
Aviinaaash <aviinaashdevkate@gmail.com>
2023-04-21 12:01:36 +0530

    auth added again

--- index.js
+++ index.js
@@ -51,9 +51,9 @@ app1.use(
 );
 app2.use(
   cors({
-       origin: "http://localhost:3001",
+      //  origin: "http://localhost:3001",
     // origin: "https://my-machine-store-dashboard.onrender.com",
-    // origin: "http://15.207.31.23:3001",
+    origin: "http://15.207.31.23:3001",
     credentials: true,
     exposedHeaders: ["Set-Cookie", "Date", "ETag"],
   })
